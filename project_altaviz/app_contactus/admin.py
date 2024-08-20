23236969dd7152a3b0548725955c302a57987129
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-20 11:18:55 +0100

    fix admin.py: fixed the registered model name on the admin interface

--- project_altaviz/app_contactus/admin.py
+++ project_altaviz/app_contactus/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ContactUser
+from .models import ContactUs
 
 # Register your models here.
-admin.site.register(ContactUser)
+admin.site.register(ContactUs)
