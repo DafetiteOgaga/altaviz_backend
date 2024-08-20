be4e1f3c8321eed0b732c5e2830c7a4db17fa2d3
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-20 10:42:44 +0100

    fix models.py: added one to one and many to one relationship to name and bank fields for custodian model

--- project_altaviz/app_bank/models.py
+++ project_altaviz/app_bank/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Bank(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return self.name
 
 class Custodian(models.Model):
-    bank = models.ForeignKey('Bank', on_delete=models.PROTECT, related_name='custodians')
+    email = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='mail')
+    bank = models.ForeignKey('Bank', on_delete=models.PROTECT, related_name='bank')
     state = models.CharField(max_length=50)
     branch = models.CharField(max_length=100)
     location = models.CharField(max_length=100, null=True, blank=True)
-    quantity_of_atm = models.IntegerField()
+    # is_deleted = models.BooleanField(default=False)
+    # def delete(self):
+    #     self.is_deleted = True
+    #     self.save()
+    def __str__(self) -> str:
+        return f'{self.state}, {self.branch}, {self.location}'
