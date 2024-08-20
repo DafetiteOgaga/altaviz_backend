d8d6d96b0f1728f536981a7eb0d599f6810a7974
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-20 11:13:01 +0100

    fix models.py: help desk model created

--- project_altaviz/app_help_desk/models.py
+++ project_altaviz/app_help_desk/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
-class Help_Desk(models.Model):
+class HelpDesk(models.Model):
     engineer = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-    is_deleted = models.BooleanField(default=False)
+    # is_deleted = models.BooleanField(default=False)
 
-    def delete(self):
-        self.is_deleted = True
-        self.save()
+    # def delete(self):
+    #     self.is_deleted = True
+    #     self.save()
