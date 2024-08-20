67b4c68454771586cfa8fcebb4a1ced75f0d813b
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-20 11:09:58 +0100

    fix urls: created routes for parts and components forms

--- project_altaviz/app_inventory/urls.py
+++ project_altaviz/app_inventory/urls.py
@@ -5,4 +5,15 @@ app_name = "app_inventory"
 
 urlpatterns = [
 	# Create your urlpatterns here.
+	path('components/', view=views.components, name='component_list'),
+	path('components/<int:pk>/', view=views.components, name='component_details'),
+
+	path('component-name/', view=views.componentName, name='componentName'),
+	path('component-name/<int:pk>/', view=views.componentName, name='componentName-details'),
+
+	path('parts/', view=views.parts, name='part_list'),
+	path('parts/<int:pk>/', view=views.parts, name='part_details'),
+
+	path('part-name/', view=views.partName, name='partName'),
+	path('part-name/<int:pk>/', view=views.partName, name='partName-details'),
 ]
