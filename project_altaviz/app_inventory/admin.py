17caf41789471aca88196bd6a14fe6e37fbb604a
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-20 11:06:25 +0100

    fix admin.py: added componentname, partname to the admin interface

--- project_altaviz/app_inventory/admin.py
+++ project_altaviz/app_inventory/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+admin.site.register(ComponentName)
 admin.site.register(Component)
+admin.site.register(PartName)
 admin.site.register(Part)
