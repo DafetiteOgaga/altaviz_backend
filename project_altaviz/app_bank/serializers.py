89116089dd01ade5659e1d44fcdcd884be948261
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-11 09:29:16 +0100

    fixed serializers.py: added more serializers

--- project_altaviz/app_bank/serializers.py
+++ project_altaviz/app_bank/serializers.py
@@ -1,30 +1,22 @@
 from rest_framework import serializers
 from .models import *
+from app_location.models import Location
+from app_custodian.models import Branch
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 # Create your serializers here.
+class StateSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = State
+		fields = '__all__'
+
+class StateNoRegionSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = State
+		fields = ['id', 'name', 'initial']
+
 class BankSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Bank
 		fields = ['id', 'name',]
-
-class CustodianSerializer(serializers.ModelSerializer):
-	bank = serializers.SlugRelatedField(
-		queryset=Bank.objects.all(), slug_field='name'
-	)
-	email = serializers.SlugRelatedField(
-		queryset=User.objects.all(), slug_field='email'
-	)
-	class Meta:
-		model = Custodian
-		fields = ['id', 'email', 'bank', 'state', 'branch', 'location',]
-	# def create(self, validated_data):
-	# 	bank = validated_data.pop('bank') # get the bank)
-	# 	email = validated_data.pop('email') # get the user email)
-	# 	custodian = Custodian.objects.create(
-	# 		bank=bank,
-	# 		email=email,
-	# 		**validated_data  # other fields
-	# 	)
-	# 	return custodian
