9e59c5679554a7e3a747c7e9b102bc9819133c1b
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-11 09:16:00 +0100

    fixed views.py: added placeholder view

--- project_altaviz/app_altaviz/views.py
+++ project_altaviz/app_altaviz/views.py
@@ -1,47 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
+from app_bank.models import *
+from app_bank.serializers import *
+from app_location.models import Location
 from .serializers import *
+from django.contrib.auth import authenticate, login, logout, get_user_model
+User = get_user_model()
 
-@api_view(['GET', 'POST'])
+# Create your views here.
+@api_view(['GET'])
 def home(request):
     return Response({"home": "welcome home from django server. This information is from the backend server"}, status=status.HTTP_200_OK)
 
-# @api_view(['GET', 'POST'])
-# def menu_list(request):
-#     if request.method == 'GET':
-#         menus = Menu.objects.all()
-#         serializer = MenuSerializer(menus, many=True)
-#         return Response(serializer.data)
-#     elif request.method == 'POST':
-#         serializer = MenuSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def menu_detail(request, pk):
-#     try:
-#         menu = Menu.objects.get(pk=pk)
-#     except Menu.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-
-#     if request.method == 'GET':
-#         serializer = MenuSerializer(menu)
-#         return Response(serializer.data)
-#     elif request.method == 'PUT':
-#         serializer = MenuSerializer(menu, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#     elif request.method == 'DELETE':
-#         menu.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-# Create your views here.
+@api_view(['GET'])
+def placeholder(request):
+    return Response({"placeholder": "This is a placeholder API endpoint."}, status=status.HTTP_200_OK)
