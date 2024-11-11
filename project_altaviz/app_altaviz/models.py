ab23d29bce5008c9bc10a1b4b11efca431f4e807
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-11 09:13:32 +0100

    fixed models.py: added ordering attribute to querysets

--- project_altaviz/app_altaviz/models.py
+++ project_altaviz/app_altaviz/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Home(models.Model):
-    test = models.CharField(max_length=100)
+	test = models.CharField(max_length=100)
+	class Meta:
+		ordering = ['id']
