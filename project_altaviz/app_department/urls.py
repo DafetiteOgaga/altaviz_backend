0381d6eee77f663dff416f890a9c22e53f4f069b
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-11 10:21:14 +0100

    new updates

--- project_altaviz/app_department/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "app_department"
-
-urlpatterns = [
-	# Create your urlpatterns here.
-	path('departments/', view=views.departments, name='department'),
-]
--- project_altaviz/app_requests_and_posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "app_requests_and_posts"
-
-urlpatterns = [
-	# Create your urlpatterns here.
-]
