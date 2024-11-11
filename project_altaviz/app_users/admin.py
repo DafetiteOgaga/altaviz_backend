5675376981fada202fb2aeac25b24f2981f29320
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-11 10:12:32 +0100

    fixed admin.py: added engineer, region, engineerassignmentnotification and updatelocationandbranchnotification models to the admin dashboard

--- project_altaviz/app_users/admin.py
+++ project_altaviz/app_users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import *
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
@@ -8,9 +8,9 @@ class CustomUserAdmin(UserAdmin):
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('username', 'first_name', 'last_name',
                                         'middle_name', 'phone', 'wphone',
-                                        'address', 'department', 'deliveries',
-                                        'pendings', 'is_deleted', 'profile_picture',
-                                        'aboutme',)}),
+                                        'address', 'gender', 'dob', 'role', 'region',
+                                        'location', 'state', 'pendings', 'is_deleted',
+                                        'profile_picture', 'aboutme',)}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
                                     'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
@@ -19,13 +19,16 @@ class CustomUserAdmin(UserAdmin):
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'username', 'password1', 'password2', 'first_name',
-                        'last_name', 'middle_name', 'phone', 'wphone', 'address',
-                        'department', 'deliveries', 'pendings', 'is_deleted',
-                        'profile_picture', 'aboutme',),
+                        'last_name', 'middle_name', 'phone', 'wphone', 'address', 'gender',
+                        'dob',    'role', 'region', 'location', 'state', 'pendings',
+                        'is_deleted', 'profile_picture', 'aboutme',),
         }),
     )
     search_fields = ('email',)
     # search_fields = ('email', 'username',)
     ordering = ('email',)
-    
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Engineer)
+admin.site.register(Region)
+admin.site.register(EngineerAssignmentNotificaion)
+admin.site.register(UpdateLocationAndBranchNotification)
