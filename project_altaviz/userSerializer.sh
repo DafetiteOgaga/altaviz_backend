996fc35894d6a5592f53c80ca740d404fdea4653
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-12-21 23:13:25 +0100

    userSerializer.sh

---
+++ project_altaviz/userSerializer.sh
@@ -0,0 +1,67 @@
+
+# python syntax
+# definition
+def printEvenNumb(num1, num2):
+    print(num1+num2)
+
+# execution or invocation or call
+printEvenNumb(4, 9)
+printEvenNumb(7, 12)
+
+# () parenthesis
+# [] square brackets
+# <> angle brackets
+# {} curly braces
+
+
+# bash syntac=x
+# loop
+for ... do
+done
+
+# function
+function printMe () {
+    echo "this is a function"
+}
+
+printMe
+
+# conditional
+if ... then
+elif ... then
+elif ... then
+elif ... then
+elif ... then
+else
+
+bingo="cat"
+if [[ $bingo=="dog" ]]; then
+    echo "yes"
+elif [[ $bingo=="cat" ]]; then
+    echo "meow"
+else
+    echo "neither"
+fi
+
+a == b : true or false
+a != b : true or false
+a < b : true or false
+a > b : true or false
+a <= b : true or false
+a >= b : true or false
+# comparison
+# =  ==> assignment
+# == ==> equality
+# !=  ==> not equal
+# > ==> greater than
+# < ==> less than
+# >= ==> greater than or equal to
+# <= ==> less than or equal to
+
+
+# -eq ==> equal
+# -ne ==> not equal
+# -lt ==> less than
+# -le ==> less than or equal
+# -gt ==> greater than
+# -ge ==> greater than or equal
