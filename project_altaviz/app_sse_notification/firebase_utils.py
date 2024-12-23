d2a96ab415935c7cb99dc74df57320322e55d385
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-12-23 10:28:07 +0100

    fixed firebase_utils.py: added configuration for chats notification

--- project_altaviz/app_sse_notification/firebase_utils.py
+++ project_altaviz/app_sse_notification/firebase_utils.py
@@ -1,32 +1,43 @@
 from datetime import datetime
 import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials, db, initialize_app, _apps
 import os
 
 # Dynamically construct the path to the credentials file
 homePath = os.path.expanduser('~')
-firebase_cred_path = os.path.join(homePath, 'firebase-admin.json')
+firebase_cred_path = os.path.join(homePath, 'firebase')
 
-# firebase_utils.py
-if not firebase_admin._apps:
-	cred = credentials.Certificate(firebase_cred_path)  # Adjust the path
-	firebase_admin.initialize_app(cred, {
+notification_db = None
+chats_db = None
+# notifications
+# if not firebase_admin._apps:
+
+
+# Check and initialize notification app
+if "notification_db" not in _apps:
+	cred_notification = credentials.Certificate(f'{firebase_cred_path}/altaviz-app-notification.json')  # Adjust the path
+	notification_db = firebase_admin.initialize_app(cred_notification, {
 		"databaseURL": "https://altaviz-notifications-default-rtdb.firebaseio.com/"
-	})
+	}, name="notification_db")
+
+# Check and initialize chat app
+if "chats_db" not in _apps:
+	cred_chats = credentials.Certificate(f'{firebase_cred_path}/altaviz-chat-notification.json')  # Adjust the path
+	chats_db = firebase_admin.initialize_app(cred_chats, {
+		"databaseURL": "https://altaviz-chat-notification-default-rtdb.firebaseio.com/"
+	}, name="chats_db")
+
 
 def send_notification(message):
-	"""
-	Send a notification to Firebase Realtime Database.
-
-	Args:
-		user_id (int): The ID of the user to notify.
-		message (str): The notification message.
-	"""
 	print('Sending notification to Firebase Realtime Database')
 	# print(f'user_id: {user_id}')
 	print(f'message: {message}')
+	notification_data = {
+		"message": message,
+		"timestamp": datetime.now().isoformat()
+	}
 	# Define the path in Firebase
-	ref = db.reference(f"notifications")
+	ref = db.reference(f"notifications", app=notification_db)
 	print(f'the referenced location: {ref}')
 	data = ref.get()
 	print("Data at the reference:", data)  # Prints the data stored at the reference
@@ -39,11 +50,55 @@ def send_notification(message):
 		print(f"Old notifications for {dataKey} have been deleted.")
 
 		# Push the new notification to Firebase
-		ref.push({
-			"message": message,
-			"timestamp": datetime.now().isoformat()
-		})
+		ref.push(notification_data)
 		print(f"New notification sent to users.")
+	except Exception as e:
+		print(f"Error while sending notification to users: {e}")
+
+
+def send_chat_notification(senderID, receiverID, message, senderName, receiverName):
+	print(f'Sending chat notifications from {senderName} to {receiverName} using Firebase Realtime Database')
+	# print(f'user_id: {user_id}')
+	print(f'message: {message}')
+	# Define the path in Firebase
+	ref = db.reference(f"chat-notifications/{receiverID}", app=chats_db)
+	print(f'the referenced location: {ref}')
+	data = ref.get()
+	print("Data at the reference:", data)  # Prints the data stored at the reference
+	counter = 0
+	dataKey = None
+	senders = {}
+	listKeys = None
+	if data:
+		dataKey = next(iter(data.keys()), None) if data else None # gets the first key from the reference dict
+		print(f'dataKey: {dataKey}')
+		listKeys = str(senderID) in data[dataKey]['sendersList'].keys()
+		print(f'senderID: {senderID}')
+		print(f'previous sender: {listKeys}')
+		if listKeys:
+			# counter = data[dataKey]['sendersList'][senderID]['notificationCount']
+			counter = data[dataKey]['sendersList'][f'{senderID}']['notificationCount']
+		senders = data[dataKey]['sendersList']
+	print(f'counter: {counter+1}')
+	print(f'senders: {senders}')
+	notification_data = {
+		"receiverID": receiverID,
+		'sendersList': {
+			**senders,
+			**{senderID: {'notificationCount': counter + 1}}
+		},
+		"notificationTimestamp": datetime.now().isoformat()
+	}
+
+	# Push and replace the notification in Firebase
+	try:
+		# Delete old data for the user
+		ref.delete()
+		print(f"Old notifications for {dataKey} have been deleted.")
+
+		# Push the new notification to Firebase
+		ref.push(notification_data)
+		print(f"New notification sent to {receiverName}.")
 	
 	except Exception as e:
 		print(f"Error while sending notification to users: {e}")
