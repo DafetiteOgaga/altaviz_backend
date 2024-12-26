d4b2707f584e5b01d7029ab35a2bceadb72e35e1
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-12-26 21:51:38 +0100

    fixed serializers.py: created the BankBranchSerializer model to handle serialization of bank objects

--- project_altaviz/app_bank/serializers.py
+++ project_altaviz/app_bank/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import *
 from app_location.models import Location
 from app_custodian.models import Branch
+# from app_custodian.serializers import BranchListSerializer
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -20,3 +21,15 @@ class BankSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Bank
 		fields = ['id', 'name',]
+
+class BankBranchSerializer(serializers.ModelSerializer):
+	locations = serializers.SerializerMethodField()
+	class Meta:
+		model = Bank
+		fields = ['id', 'name', 'locations']
+
+	def get_locations(self, instance):
+		# Lazy import inside the method
+		from app_location.serializers import LocationBranches
+		locations = instance.banklocations.all()
+		return LocationBranches(locations, many=True).data
