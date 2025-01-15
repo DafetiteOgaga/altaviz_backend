1a61b547dfe3207bb65ed0eae85d621d6d93de7d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-01-15 15:39:49 +0100

    fixed urls.py: added password update functionality endpoint

--- project_altaviz/app_auth/urls.py
+++ project_altaviz/app_auth/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	# Create your urlpatterns here.
 	path('login/', view=views.loginView, name='loginView'),
     path('logout/', view=views.logoutView, name='logoutView'),
+    path('change-password/<int:pk>/', view=views.changePassword, name='changePassword'),
     path('check-auth/', view=views.checkAuth, name='checkAuth'),
 ]
