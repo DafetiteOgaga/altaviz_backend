b3ca3d661e5e599793e00c54c3746acab5b7656d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-03-17 15:02:24 +0100

    Bumped version to v20250317.1502

--- project_altaviz/app_altaviz/views.py
+++ project_altaviz/app_altaviz/views.py
@@ -28,6 +28,6 @@ def getAllAccounts(request):
 
 @api_view(['GET'])
 def versionNumber(request):
-    versionDict = {'version': 'v20250317.1308'}
+    versionDict = {'version': 'v20250317.1502'}
     print("versionDict: ", versionDict)
     return Response(versionDict, status=status.HTTP_200_OK)
